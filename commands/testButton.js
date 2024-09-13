a16bff16ca8ada8d164b071380f7c868b71d9cdf
famq <143351292+famque@users.noreply.github.com>
2024-09-13 07:14:02 -0400

    Update testButton.js

--- commands/testButton.js
+++ commands/testButton.js
@@ -11,16 +11,14 @@ module.exports = {
     .setDescription("Test a button interaction"),
 
   async execute(interaction) {
-    // Create a button
+    
     const button = new ButtonBuilder()
-      .setCustomId("test_button") // Unique ID for the button
+      .setCustomId("test_button")
       .setLabel("Click Me!")
       .setStyle(ButtonStyle.Primary);
 
-    // Create an action row to hold the button
     const row = new ActionRowBuilder().addComponents(button);
 
-    // Send the reply with the button
     await interaction.reply({
       content: "Here is a button for you to test!",
       components: [row],
