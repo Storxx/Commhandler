93d9fbf6f36319c53765a849da63de164e98a11e
Storxx <andrewcastro100301@gmail.com>
2025-08-04 12:40:26 -0700

    Comm-Handler
    Folder

--- handlers/buttonHandlers/testButtonHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (interaction) => {
-  if (interaction.isButton()) {
-    if (interaction.customId === "test_button") {
-      await interaction.reply({ content: "Button clicked!", ephemeral: true });
-    }
-  }
-};
